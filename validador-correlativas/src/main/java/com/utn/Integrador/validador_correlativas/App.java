46322536c2f69d4d20d7a884cb15dd3890e026dc
Brenda <brentorres26@gmail.com>
2023-04-06 18:22:17 -0300

    Creacion proyecto validador de correlativas con maven y manejo de dependencias

---
+++ validador-correlativas/src/main/java/com/utn/Integrador/validador_correlativas/App.java
@@ -0,0 +1,13 @@
+package com.utn.Integrador.validador_correlativas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
